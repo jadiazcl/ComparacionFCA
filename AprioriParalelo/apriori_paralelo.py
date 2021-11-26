9d24eccab6d6fd165bb7c2ecd61c42475b1b801b
JDiaz-xWing <jorge.diazma@sansano.usm.cl>
2021-11-26 02:16:48 -0300

    update

---
+++ AprioriParalelo/apriori_paralelo.py
@@ -0,0 +1,193 @@
+
+import sys
+from itertools import chain, combinations
+from functools import reduce
+from time import time
+from datetime import datetime
+import json
+import ray
+import  multiprocessing as mp
+
+def memory_usage_psutil():
+    # return the memory usage in MB
+    import psutil
+    import os
+    process = psutil.Process(os.getpid())
+    #print(process.memory_info())
+    mem = process.memory_info().rss / float(2 ** 20)
+    return mem
+
+@ray.remote(num_cpus=2)
+class diccionario(object):
+    def __init__(self):
+        self.dic=dict()
+        self.cont=0
+    def add(self,llave,valor):
+        if valor not in self.dic[llave]:
+            self.dic[llave].add(valor)
+        self.cont+=1
+    def add_all(self,dics):
+        self.dic.update(dics)
+    def suma_1(self):
+        self.cont+=1
+    def get_value(self,llave):        
+        if llave in self.dic.keys():
+            type(self.dic[llave])
+            return self.dic[llave]
+        else:
+            return {}
+    def get_value_2(self,llave):
+      if llave in self.dic.keys():            
+            return {i:0 for i in self.dic[llave]},
+      else:
+          return dict()
+    def all(self):
+        return self.dic
+    def get_intent(self):
+        return self.cont
+    def verificar_agregar(self,llave,valor):
+        if llave not in self.dic:
+          self.dic[llave]=set([])        
+        self.dic[llave].add(valor)                  
+
+def trabajo_paralelo(Lk,T,I,levels,flag):
+  n_aux=0
+  if flag:
+    aux_dic=dict()
+    for P in Lk:
+      for i in I:
+        Pc = operador_clausura(P.union(set([i])), T, I)
+        n_aux+=1
+        if len(Pc) not in aux_dic:
+          aux_dic[len(Pc)] = set([])
+        aux_dic[len(Pc)].add(frozenset(Pc))          
+        #levels.verificar_agregar.remote(len(Pc),frozenset(Pc))      
+    return n_aux,aux_dic
+    
+  else: 
+    for P in Lk:
+      for i in I:
+        Pc = operador_clausura(P.union(set([i])), T, I)
+        n_aux+=1
+        if len(Pc) not in levels:
+          levels[len(Pc)] = set([])
+        levels[len(Pc)].add(frozenset(Pc))
+    return n_aux,levels
+
+
+def operador_clausura(P, T, I):
+  Pp = [t for t in T if P.issubset(t)] #P'
+  if not bool(Pp):
+    return set(I)
+  return set(reduce(set.intersection, Pp))
+
+
+
+
+def genClosedCandidates(levels, k, T, I,test_remote,NCLOSURES):
+  #Lk =ray.get(levels.get_value.remote(k-1))
+  Lk = levels.get(k-1, {})
+  largo=len(Lk)
+  Lk=list(Lk)
+  nucleos=mp.cpu_count()-1
+  procesos=[]
+  alpha=int(largo/nucleos)
+  modulo=largo%nucleos      
+
+  if alpha <1:    
+    n_aux,levels=trabajo_paralelo(Lk,T,I,levels,False)
+    NCLOSURES+=n_aux
+          
+  else:
+    j=0    
+    blocks_ids=[]      
+    for i in range(0,modulo):        
+        blocks_ids.append(test_remote.remote(Lk[j:j+alpha+1],T,I,levels,True))
+        j=j+alpha+1
+    for i in range(0,nucleos-modulo):           
+        blocks_ids.append(test_remote.remote(Lk[j:j+alpha],T,I,levels,True))
+        j=j+alpha
+    result=ray.get(blocks_ids)
+    for i,j in result:      
+      NCLOSURES+=i
+      for w,t in j.items():
+        if w not in levels.keys():
+          levels[w] = set([])
+        levels[w].update(t)
+
+  #value=ray.get(levels.get_value_2.remote(k))  
+  if k in levels:
+    return {i:0 for i in levels[k]},NCLOSURES
+  else:
+    return {},NCLOSURES
+  
+  
+
+
+
+def a_priori_closed(T, I, sigma,test_remote):
+  tid = {i:set() for i in I}
+  for ti, t in enumerate(T):
+    for i in t:
+      tid[i].add(ti)
+
+  #levels = diccionario.remote()
+  #levels.verificar_agregar.remote(0,frozenset([]))
+  NCLOSURES=0
+  levels = {0:set([frozenset([])])} # Agregamos el conjunto vacío que siempre es frecuente
+  for i, it in tid.items():
+    if len(it) >= sigma:
+      P = set([i])
+      Pc = operador_clausura(P, T, I)      
+      NCLOSURES+=1
+      #levels.verificar_agregar.remote(len(Pc),frozenset(Pc))
+      if len(Pc) not in levels:
+        levels[len(Pc)] = set([])
+      levels[len(Pc)].add(frozenset(Pc))
+
+
+  C = {}
+  i = 1
+  while i < len(I):
+    C[i+1], NCLOSURES= genClosedCandidates(levels, i+1, T, I,test_remote,NCLOSURES)    
+    i += 1
+  memoria=memory_usage_psutil()
+  return levels,NCLOSURES,memoria
+
+
+test_remote= ray.remote(trabajo_paralelo)
+print("INIT")
+start_time = time()     
+path = sys.argv[1]
+with open(path, 'r') as fin:
+    lines = fin.readlines()
+    ctx = [set(map(int, line.split())) for line in lines]
+M = set(reduce(set.union, ctx))
+
+FC_sigma,NCLOSURES,memoria = a_priori_closed(ctx, M, 0,test_remote)
+time=time()-start_time
+print("END")
+print(time)
+#resultados=ray.get(FC_sigma.all.remote())
+resultados=FC_sigma
+largo=0        
+for i,j in resultados.items():
+  #print("nivel ",i)
+  for w in j:
+    #print('\t', sorted(w))
+    largo+=1
+#print("Largo: ",largo)
+
+results = {
+    'n_results' : largo,
+    'n_closures' : NCLOSURES,
+    'exec_time' : time,  
+    'memory_info': memoria                              
+}
+
+d = datetime.now()
+timestamp = '{}{}{}-{}-{}-{}'.format(d.year, d.month, d.day, d.hour, d.minute, d.second)
+fname = path[path.rfind('/'):path.rfind('.')]
+fname=fname[1:]
+with open('results/{}-{}-{}.json'.format("apriori_ray_resurrection",fname, timestamp) , 'w') as fout:
+    json.dump(results, fout)
--- execute.sh
+++ execute.sh
@@ -1,20 +1,76 @@
 #!/bin/sh
-python3 apriori_ray_resurrection.py ../data/mushroom.txt 
-echo "1"
-python3 apriori_ray_resurrection.py ../data/flight.txt 
-echo "2"
-python3 raypy/ray_miner.v7.py ../data/mushroom.txt 8
-echo "3"
-python3 raypy/ray_miner.v7.py ../data/mushroom.txt 25
-echo "3.1"
-python3 raypy/ray_miner.v7.py ../data/mushroom.txt 20
-echo "4"
-python3 raypy/ray_miner.v7.py ../data/mushroom.txt 30
-echo "5"
-python3 raypy/ray_miner.v7.py ../data/flight.txt 8
-echo "6"
-python3 raypy/ray_miner.v7.py ../data/flight.txt 20
-echo "8"
-python3 raypy/ray_miner.v7.py ../data/flight.txt 25
-echo "9"
-python3 raypy/ray_miner.v7.py ../data/flight.txt 30
+echo "Apriori Execute"
+echo "---------------"
+echo "Mushroom"
+python3 Apriori/apriori.py ../Datasets/mushroom.txt 
+echo "Flight"
+python3 Apriori/apriori.py ../Datasets/flight.txt 
+echo "Adult"
+python3 Apriori/apriori.py ../Datasets/mushroom.txt 
+echo "Car"
+python3 Apriori/apriori.py ../Datasets/car.txt 
+echo "Credit"
+python3 Apriori/apriori.py ../Datasets/credit.txt 
+echo "Horse"
+python3 Apriori/apriori.py ../Datasets/horse.txt 
+echo "Servo"
+python3 Apriori/apriori.py ../Datasets/servo_inv.txt 
+echo "Credo"
+python3 Apriori/apriori.py ../Datasets/credo_cran_cran_context.cxt 
+echo "---------------"
+echo "Apriori Parallel Execute"
+echo "---------------"
+echo "Mushroom"
+python3 AprioriParalelo/apriori_paralelo.py ../Datasets/mushroom.txt 
+echo "Flight"
+python3 AprioriParalelo/apriori_paralelo.py ../Datasets/flight.txt 
+echo "Adult"
+python3 AprioriParalelo/apriori_paralelo.py ../Datasets/mushroom.txt 
+echo "Car"
+python3 AprioriParalelo/apriori_paralelo.py ../Datasets/car.txt 
+echo "Credit"
+python3 AprioriParalelo/apriori_paralelo.py ../Datasets/credit.txt 
+echo "Horse"
+python3 AprioriParalelo/apriori_paralelo.py ../Datasets/horse.txt 
+echo "Servo"
+python3 AprioriParalelo/apriori_paralelo.py ../Datasets/servo_inv.txt 
+echo "Credo"
+python3 AprioriParalelo/apriori_paralelo.py ../Datasets/credo_cran_cran_context.cxt 
+echo "---------------"
+echo "NextClosure Execute"
+echo "---------------"
+echo "Mushroom"
+python3 NextClosure/next_closure.py ../Datasets/mushroom.txt 
+echo "Flight"
+python3 NextClosure/next_closure.py ../Datasets/flight.txt 
+echo "Adult"
+python3 NextClosure/next_closure.py ../Datasets/mushroom.txt 
+echo "Car"
+python3 NextClosure/next_closure.py ../Datasets/car.txt 
+echo "Credit"
+python3 NextClosure/next_closure.py ../Datasets/credit.txt 
+echo "Horse"
+python3 NextClosure/next_closure.py ../Datasets/horse.txt 
+echo "Servo"
+python3 NextClosure/next_closure.py ../Datasets/servo_inv.txt 
+echo "Credo"
+python3 NextClosure/next_closure.py ../Datasets/credo_cran_cran_context.cxt 
+echo "---------------"
+echo "Paralectico Execute"
+echo "---------------"
+echo "Mushroom"
+python3 Paralectico/paralectico-v1.py ../Datasets/mushroom.txt 
+echo "Flight"
+python3 Paralectico/paralectico-v1.py ../Datasets/flight.txt 
+echo "Adult"
+python3 Paralectico/paralectico-v1.py ../Datasets/mushroom.txt 
+echo "Car"
+python3 Paralectico/paralectico-v1.py ../Datasets/car.txt 
+echo "Credit"
+python3 Paralectico/paralectico-v1.py ../Datasets/credit.txt 
+echo "Horse"
+python3 Paralectico/paralectico-v1.py ../Datasets/horse.txt 
+echo "Servo"
+python3 Paralectico/paralectico-v1.py ../Datasets/servo_inv.txt 
+echo "Credo"
+python3 Paralectico/paralectico-v1.py ../Datasets/credo_cran_cran_context.cxt 
