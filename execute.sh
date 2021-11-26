fa0a94c97d16e487243a888d69549d94211224ad
JDiaz-xWing <jorge.diazma@sansano.usm.cl>
2021-11-26 02:50:21 -0300

    ready execute

--- execute.sh
+++ execute.sh
@@ -2,134 +2,134 @@
 echo "Apriori Execute"
 echo "---------------"
 echo "Mushroom"
-#python3 Apriori/apriori.py Datasets/mushroom.txt 
+python3 Apriori/apriori.py Datasets/mushroom.txt 
 echo "Flight"
-#python3 Apriori/apriori.py Datasets/flight.txt 
+python3 Apriori/apriori.py Datasets/flight.txt 
 echo "Adult"
 python3 Apriori/apriori.py Datasets/adult.txt 
 echo "Car"
-#python3 Apriori/apriori.py Datasets/car.txt 
+python3 Apriori/apriori.py Datasets/car.txt 
 echo "Credit"
-#python3 Apriori/apriori.py Datasets/credit.txt 
+python3 Apriori/apriori.py Datasets/credit.txt 
 echo "Horse"
-#python3 Apriori/apriori.py Datasets/horse.txt 
+python3 Apriori/apriori.py Datasets/horse.txt 
 echo "Servo"
-#python3 Apriori/apriori.py Datasets/servo_inv.txt 
+python3 Apriori/apriori.py Datasets/servo_inv.txt 
 echo "Credo"
-#python3 Apriori/apriori.py Datasets/credo_cran_cran_context.cxt 
+python3 Apriori/apriori.py Datasets/credo_cran_cran_context.cxt 
 echo "---------------"
 echo "Apriori Parallel Execute"
 echo "---------------"
 echo "Mushroom"
-#python3 AprioriParalelo/apriori_paralelo.py Datasets/mushroom.txt 
+python3 AprioriParalelo/apriori_paralelo.py Datasets/mushroom.txt 
 echo "Flight"
-#python3 AprioriParalelo/apriori_paralelo.py Datasets/flight.txt 
+python3 AprioriParalelo/apriori_paralelo.py Datasets/flight.txt 
 echo "Adult"
 python3 AprioriParalelo/apriori_paralelo.py Datasets/adult.txt 
 echo "Car"
-#python3 AprioriParalelo/apriori_paralelo.py Datasets/car.txt 
+python3 AprioriParalelo/apriori_paralelo.py Datasets/car.txt 
 echo "Credit"
-#python3 AprioriParalelo/apriori_paralelo.py Datasets/credit.txt 
+python3 AprioriParalelo/apriori_paralelo.py Datasets/credit.txt 
 echo "Horse"
-#python3 AprioriParalelo/apriori_paralelo.py Datasets/horse.txt 
+python3 AprioriParalelo/apriori_paralelo.py Datasets/horse.txt 
 echo "Servo"
-#python3 AprioriParalelo/apriori_paralelo.py Datasets/servo_inv.txt 
+python3 AprioriParalelo/apriori_paralelo.py Datasets/servo_inv.txt 
 echo "Credo"
-#python3 AprioriParalelo/apriori_paralelo.py Datasets/credo_cran_cran_context.cxt 
+python3 AprioriParalelo/apriori_paralelo.py Datasets/credo_cran_cran_context.cxt 
 echo "---------------"
 echo "NextClosure Execute"
 echo "---------------"
 echo "Mushroom"
-#python3 NextClosure/next_closure.py Datasets/mushroom.txt 
+python3 NextClosure/next_closure.py Datasets/mushroom.txt 
 echo "Flight"
-#python3 NextClosure/next_closure.py Datasets/flight.txt 
+python3 NextClosure/next_closure.py Datasets/flight.txt 
 echo "Adult"
 python3 NextClosure/next_closure.py Datasets/adult.txt 
 echo "Car"
-#python3 NextClosure/next_closure.py Datasets/car.txt 
+python3 NextClosure/next_closure.py Datasets/car.txt 
 echo "Credit"
-#python3 NextClosure/next_closure.py Datasets/credit.txt 
+python3 NextClosure/next_closure.py Datasets/credit.txt 
 echo "Horse"
-#python3 NextClosure/next_closure.py Datasets/horse.txt 
+python3 NextClosure/next_closure.py Datasets/horse.txt 
 echo "Servo"
-#python3 NextClosure/next_closure.py Datasets/servo_inv.txt 
+python3 NextClosure/next_closure.py Datasets/servo_inv.txt 
 echo "Credo"
-#python3 NextClosure/next_closure.py Datasets/credo_cran_cran_context.cxt 
+python3 NextClosure/next_closure.py Datasets/credo_cran_cran_context.cxt 
 echo "---------------"
 echo "Paralectico Execute"
 echo "---------------"
 echo "Mushroom"
-#python3 Paralectico/paralectico-v1.py Datasets/mushroom.txt 5
-#python3 Paralectico/paralectico-v1.py Datasets/mushroom.txt 8
-#python3 Paralectico/paralectico-v1.py Datasets/mushroom.txt 12
-#python3 Paralectico/paralectico-v1.py Datasets/mushroom.txt 15
-#python3 Paralectico/paralectico-v1.py Datasets/mushroom.txt 18
-#python3 Paralectico/paralectico-v1.py Datasets/mushroom.txt 20
+python3 Paralectico/paralectico-v1.py Datasets/mushroom.txt 5
+python3 Paralectico/paralectico-v1.py Datasets/mushroom.txt 8
+python3 Paralectico/paralectico-v1.py Datasets/mushroom.txt 12
+python3 Paralectico/paralectico-v1.py Datasets/mushroom.txt 15
+python3 Paralectico/paralectico-v1.py Datasets/mushroom.txt 18
+python3 Paralectico/paralectico-v1.py Datasets/mushroom.txt 20
 echo "Flight"
-#python3 Paralectico/paralectico-v1.py Datasets/flight.txt 5
-#python3 Paralectico/paralectico-v1.py Datasets/flight.txt 8
-#python3 Paralectico/paralectico-v1.py Datasets/flight.txt 12
-#python3 Paralectico/paralectico-v1.py Datasets/flight.txt 15
-#python3 Paralectico/paralectico-v1.py Datasets/flight.txt 18
-#python3 Paralectico/paralectico-v1.py Datasets/flight.txt 20
+python3 Paralectico/paralectico-v1.py Datasets/flight.txt 5
+python3 Paralectico/paralectico-v1.py Datasets/flight.txt 8
+python3 Paralectico/paralectico-v1.py Datasets/flight.txt 12
+python3 Paralectico/paralectico-v1.py Datasets/flight.txt 15
+python3 Paralectico/paralectico-v1.py Datasets/flight.txt 18
+python3 Paralectico/paralectico-v1.py Datasets/flight.txt 20
 echo "Adult"
-#python3 Paralectico/paralectico-v1.py Datasets/adult.txt 5
+python3 Paralectico/paralectico-v1.py Datasets/adult.txt 5
 python3 Paralectico/paralectico-v1.py Datasets/adult.txt 8
-#python3 Paralectico/paralectico-v1.py Datasets/adult.txt 12
-#python3 Paralectico/paralectico-v1.py Datasets/adult.txt 15
-#python3 Paralectico/paralectico-v1.py Datasets/adult.txt 18
-#python3 Paralectico/paralectico-v1.py Datasets/adult.txt 20
+python3 Paralectico/paralectico-v1.py Datasets/adult.txt 12
+python3 Paralectico/paralectico-v1.py Datasets/adult.txt 15
+python3 Paralectico/paralectico-v1.py Datasets/adult.txt 18
+python3 Paralectico/paralectico-v1.py Datasets/adult.txt 20
 echo "Car"
-#python3 Paralectico/paralectico-v1.py Datasets/car.txt 5
-#python3 Paralectico/paralectico-v1.py Datasets/car.txt 8
-#python3 Paralectico/paralectico-v1.py Datasets/car.txt 12
-#python3 Paralectico/paralectico-v1.py Datasets/car.txt 15
-#python3 Paralectico/paralectico-v1.py Datasets/car.txt 18
-#python3 Paralectico/paralectico-v1.py Datasets/car.txt 20
+python3 Paralectico/paralectico-v1.py Datasets/car.txt 5
+python3 Paralectico/paralectico-v1.py Datasets/car.txt 8
+python3 Paralectico/paralectico-v1.py Datasets/car.txt 12
+python3 Paralectico/paralectico-v1.py Datasets/car.txt 15
+python3 Paralectico/paralectico-v1.py Datasets/car.txt 18
+python3 Paralectico/paralectico-v1.py Datasets/car.txt 20
 echo "Credit"
-#python3 Paralectico/paralectico-v1.py Datasets/credit.txt 5 
-#python3 Paralectico/paralectico-v1.py Datasets/credit.txt 8
-#python3 Paralectico/paralectico-v1.py Datasets/credit.txt 12
-#python3 Paralectico/paralectico-v1.py Datasets/credit.txt 15
-#python3 Paralectico/paralectico-v1.py Datasets/credit.txt 18
-#python3 Paralectico/paralectico-v1.py Datasets/credit.txt 20
+python3 Paralectico/paralectico-v1.py Datasets/credit.txt 5 
+python3 Paralectico/paralectico-v1.py Datasets/credit.txt 8
+python3 Paralectico/paralectico-v1.py Datasets/credit.txt 12
+python3 Paralectico/paralectico-v1.py Datasets/credit.txt 15
+python3 Paralectico/paralectico-v1.py Datasets/credit.txt 18
+python3 Paralectico/paralectico-v1.py Datasets/credit.txt 20
 echo "Horse"
-#python3 Paralectico/paralectico-v1.py Datasets/horse.txt 5
-#python3 Paralectico/paralectico-v1.py Datasets/horse.txt 8
-#python3 Paralectico/paralectico-v1.py Datasets/horse.txt 12
-#python3 Paralectico/paralectico-v1.py Datasets/horse.txt 15
-#python3 Paralectico/paralectico-v1.py Datasets/horse.txt 18
-#python3 Paralectico/paralectico-v1.py Datasets/horse.txt 20
+python3 Paralectico/paralectico-v1.py Datasets/horse.txt 5
+python3 Paralectico/paralectico-v1.py Datasets/horse.txt 8
+python3 Paralectico/paralectico-v1.py Datasets/horse.txt 12
+python3 Paralectico/paralectico-v1.py Datasets/horse.txt 15
+python3 Paralectico/paralectico-v1.py Datasets/horse.txt 18
+python3 Paralectico/paralectico-v1.py Datasets/horse.txt 20
 echo "Servo"
-#python3 Paralectico/paralectico-v1.py Datasets/servo_inv.txt 5
-#python3 Paralectico/paralectico-v1.py Datasets/servo_inv.txt 8
-#python3 Paralectico/paralectico-v1.py Datasets/servo_inv.txt 12
-#python3 Paralectico/paralectico-v1.py Datasets/servo_inv.txt 15
-#python3 Paralectico/paralectico-v1.py Datasets/servo_inv.txt 18
-#python3 Paralectico/paralectico-v1.py Datasets/servo_inv.txt 20
+python3 Paralectico/paralectico-v1.py Datasets/servo_inv.txt 5
+python3 Paralectico/paralectico-v1.py Datasets/servo_inv.txt 8
+python3 Paralectico/paralectico-v1.py Datasets/servo_inv.txt 12
+python3 Paralectico/paralectico-v1.py Datasets/servo_inv.txt 15
+python3 Paralectico/paralectico-v1.py Datasets/servo_inv.txt 18
+python3 Paralectico/paralectico-v1.py Datasets/servo_inv.txt 20
 echo "Credo"
-#python3 Paralectico/paralectico-v1.py Datasets/credo_cran_cran_context.cxt 5
-#python3 Paralectico/paralectico-v1.py Datasets/credo_cran_cran_context.cxt 8
-#python3 Paralectico/paralectico-v1.py Datasets/credo_cran_cran_context.cxt 12
-#python3 Paralectico/paralectico-v1.py Datasets/credo_cran_cran_context.cxt 15
-#python3 Paralectico/paralectico-v1.py Datasets/credo_cran_cran_context.cxt 18
-#python3 Paralectico/paralectico-v1.py Datasets/credo_cran_cran_context.cxt 20
+python3 Paralectico/paralectico-v1.py Datasets/credo_cran_cran_context.cxt 5
+python3 Paralectico/paralectico-v1.py Datasets/credo_cran_cran_context.cxt 8
+python3 Paralectico/paralectico-v1.py Datasets/credo_cran_cran_context.cxt 12
+python3 Paralectico/paralectico-v1.py Datasets/credo_cran_cran_context.cxt 15
+python3 Paralectico/paralectico-v1.py Datasets/credo_cran_cran_context.cxt 18
+python3 Paralectico/paralectico-v1.py Datasets/credo_cran_cran_context.cxt 20
 echo "---------------"
 echo "Paralectico V2 Execute"
 echo "---------------"
 echo "Mushroom"
-#python3 Paralectico/paralectico-v2.py Datasets/mushroom.txt 
+python3 Paralectico/paralectico-v2.py Datasets/mushroom.txt 
 echo "Flight"
-#python3 Paralectico/paralectico-v2.py Datasets/flight.txt 
+python3 Paralectico/paralectico-v2.py Datasets/flight.txt 
 echo "Adult"
 python3 Paralectico/paralectico-v2.py Datasets/mushroom.txt 
 echo "Car"
-#python3 Paralectico/paralectico-v2.py Datasets/car.txt 
+python3 Paralectico/paralectico-v2.py Datasets/car.txt 
 echo "Credit"
-#python3 Paralectico/paralectico-v2.py Datasets/credit.txt 
+python3 Paralectico/paralectico-v2.py Datasets/credit.txt 
 echo "Horse"
-#python3 Paralectico/paralectico-v2.py Datasets/horse.txt 
+python3 Paralectico/paralectico-v2.py Datasets/horse.txt 
 echo "Servo"
-#python3 Paralectico/paralectico-v2.py Datasets/servo_inv.txt 
+python3 Paralectico/paralectico-v2.py Datasets/servo_inv.txt 
 echo "Credo"
-#python3 Paralectico/paralectico-v2.py Datasets/credo_cran_cran_context.cxt 
+python3 Paralectico/paralectico-v2.py Datasets/credo_cran_cran_context.cxt 
